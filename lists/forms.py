8e8fba3a5d112f72b436380068400a7af8498ff0
EthanK28 <kes0228@gmail.com>
2015-05-16 15:27:06 +0900

    249 Done!

--- lists/forms.py
+++ lists/forms.py
@@ -1,8 +1,10 @@
 __author__ = 'es-mac'
 from django import forms
 from lists.models import Item
+from django.core.exceptions import ValidationError
 
 EMPTY_LIST_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
     class Meta:
@@ -21,6 +23,23 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+
+
+class ExistingListItemForm(ItemForm):
+
+
+    def __init__(self, for_list, *args, **kargs):
+        super().__init__(*args, **kargs)
+        self.instanec.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+            
 
 
 
